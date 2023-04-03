de0caffce7394dde6cac8325f24f99007a0cd86b
ferrori <teadorr24@gmail.com>
2023-04-03 17:51:35 +0300

    Update MyFirstProgram.java

--- kingdom_CUM/src/main/java/ru/stqa/pft/chw/MyFirstProgram.java
+++ kingdom_CUM/src/main/java/ru/stqa/pft/chw/MyFirstProgram.java
@@ -2,8 +2,8 @@ package ru.stqa.pft.chw;
 
 public class MyFirstProgram {
 
-	public static void main (String[] args) {
-		System.out.println("Hello, world");
-	}
-	
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+
 }
