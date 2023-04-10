26e77fc73e88f5fd4ca47bd0389f40419bff20c7
ferrori <teadorr24@gmail.com>
2023-04-10 19:59:46 +0300

    Добавлено несколько функций

--- kingdom_CUM/src/main/java/ru/stqa/pft/chw/MyFirstProgram.java
+++ kingdom_CUM/src/main/java/ru/stqa/pft/chw/MyFirstProgram.java
@@ -3,7 +3,27 @@ package ru.stqa.pft.chw;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        hello("world");
+        hello("user");
+        hello("cum");
+
+        double l = 5;
+        System.out.println("Прощадь квадрата со стороной " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Прощадь прямоугольника со сторонами " + a + " и " + b  + " = " + area(a,b));
     }
 
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double len) {
+       return len * len;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
 }
